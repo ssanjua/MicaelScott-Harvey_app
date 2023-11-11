6f3eb67884e21eb788b264ce902663a5933ad316
paupallares <ppaupallares@gmail.com>
2023-11-11 16:40:53 +1000

    implementing recording

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, jsonify
+import speech_recognition as sr
+import os
 
 app = Flask(__name__)
 
@@ -8,6 +9,33 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+
+@app.route('/audio-to-text', methods=['POST'])
+def audio_to_text():
+    if 'audioBlob' not in request.files:
+        return jsonify({'error': 'No file part'}), 400
+
+    # Guardar el archivo temporalmente en el servidor
+    audio_blob = request.files['audioBlob']
+    audio_filename = 'temp_audio.wav'
+    audio_blob.save(audio_filename)
+
+    # Procesar el archivo de audio
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(audio_filename) as source:
+        audio_data = recognizer.record(source)
+        try:
+            # Intenta reconocer el audio
+            text = recognizer.recognize_google(audio_data)
+            return jsonify({'text': text})
+        except sr.UnknownValueError:
+            return jsonify({'error': 'Speech recognition could not understand audio'}), 400
+        except sr.RequestError as e:
+            return jsonify({'error': 'Could not request results from the speech recognition service'}), 500
+        finally:
+            # Asegúrate de eliminar el archivo de audio temporal
+            if os.path.exists(audio_filename):
+                os.remove(audio_filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,17 +1,18 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const toggleButton = document.getElementById('toggleInputButton');
     const inputContainer = document.getElementById('inputContainer');
-    const textInput = document.getElementById('textInput');
+    const textInput = document.getElementById('textToSpeak');
 
     toggleButton.addEventListener('click', () => {
-        inputContainer.style.display = inputContainer.style.display === 'none' ? 'block' : 'none';
-        if (inputContainer.style.display === 'block') {
-            document.getElementById('textInput').focus();
+        if (inputContainer.style.display === 'none' || !inputContainer.style.display) {
+            inputContainer.style.display = 'block';
+            setTimeout(() => {
+                textToSpeak.focus();
+            }, 0);
+        } else {
+            inputContainer.style.display = 'none';
         }
     });
-
-    // Añade un listener para tu botón de hablar aquí
-    speakButton.addEventListener('click', speakText);
 });
 
 function speakText() {
@@ -33,3 +34,60 @@ function speakText() {
 
     synth.speak(utterance);
 }
+
+let mediaRecorder;
+let audioChunks = [];
+let audioBlob;
+
+document.getElementById('startRecordingButton').addEventListener('click', () => {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(stream => {
+            mediaRecorder = new MediaRecorder(stream);
+            audioChunks = [];
+
+            mediaRecorder.addEventListener('dataavailable', event => {
+                audioChunks.push(event.data);
+            });
+
+            mediaRecorder.addEventListener('stop', () => {
+                audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
+                console.log(audioBlob.type);
+                const audioUrl = URL.createObjectURL(audioBlob);
+                document.getElementById('audioPlayback').src = audioUrl;
+                document.getElementById('sendAudioButton').disabled = false;
+            });
+
+            mediaRecorder.start();
+            document.getElementById('stopRecordingButton').disabled = false;
+        })
+        .catch(e => {
+            console.log('No se pudo acceder al micrófono: ', e);
+        });
+});
+
+document.getElementById('stopRecordingButton').addEventListener('click', () => {
+    if (mediaRecorder.state === 'recording') {
+        mediaRecorder.stop();
+        document.getElementById('stopRecordingButton').disabled = true;
+    }
+});
+
+// Evento para el botón de enviar audio
+document.getElementById('sendAudioButton').addEventListener('click', () => {
+    let formData = new FormData();
+    formData.append('audioBlob', audioBlob, 'recording.wav');
+    
+    fetch('/audio-to-text', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data.text); // Aquí puedes hacer algo con el texto reconocido
+    })
+    .catch(error => {
+        console.error(error);
+    });
+});
+
+
