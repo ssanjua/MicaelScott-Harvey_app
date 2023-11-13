79815ac19dcce69d42ddd6ebf292e0e0cc2ba43d
paupallares <ppaupallares@gmail.com>
2023-11-14 08:35:28 +1000

    including bootstrap

--- app.py
+++ app.py
@@ -1,20 +1,22 @@
-from flask import Flask, render_template, request, jsonify
-import speech_recognition as sr
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/contact")
 def contact():
     pass
 
+
 @app.route("/about")
 def about():
     pass
+
 
 if __name__ == "__main__":
     app.run(debug=True)
